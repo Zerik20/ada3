c098d8e0b8596540dde2febe78fbbf00eadc41a4
Zerik20 <erikaban14@gmail.com>
2021-12-13 14:28:24 -0600

    ADA3 Erik Aban

---
+++ resources/views/nombre.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nombre</title>
+</head>
+<body>
+    <div>
+        <form>
+            <h1>
+            <ul>Nombre</ul>
+            </h1>
+            <input type="text">
+        </form>
+
+    </div>
+    <div class=class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+        Bienvenido {{$name}}
+    </div>
+</body>
+</html>
